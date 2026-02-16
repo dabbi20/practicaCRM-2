8b9dc774977dfc721433710020a050d56c551378
David Carrasco <davidack123456789@gmail.com>
2026-02-16 14:31:10 -0500

    Generamos una clase class, asignamos el constructor con los parametros necesarios para nuestros clientes y generamos pruebas de que este funcionando como quiero

---
+++ crm-lite/crm-lire.js
@@ -0,0 +1,19 @@
+/*
+CLASE DE CLIENTE CON LA ESTRUCTURA QUE VENDRAN NUESTROS CLIENTES YA DISEÑADA
+*/
+class Client{
+    constructor(nombre,email,cell,servicio,mensaje,contacted = false,id = null,created_at = new Date().toISOString() ){
+        this.nombre = nombre;
+        this.email = email;
+        this.cell = cell;
+        this.servicio = servicio;
+        this.mensaje = mensaje;
+        this.contacted = contacted;
+        this.id = id;
+        this.created_at = created_at;
+        
+    }
+}
+
+let cliente1 = new Client("David Carrasco","davidack123456789@gmail.com","3134476364","Diseño web", "por favor contactame al cell para negociar")
+console.log(cliente1)
