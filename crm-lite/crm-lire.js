bc4b09b72f06eab213786214f1540fad1ab184e0
David Carrasco <davidack123456789@gmail.com>
2026-02-18 20:24:55 -0500

    cree la funcion inNonEmotyString que se ecarga de validar que el texto no venga vacio y venga de forma correcta

--- crm-lite/crm-lire.js
+++ crm-lite/crm-lire.js
@@ -2,24 +2,36 @@
 CLASE DE CLIENTE CON LA ESTRUCTURA QUE VENDRAN NUESTROS CLIENTES YA DISEÑADA
 */
 class Client{
-    constructor(nombre,email,cell,servicio,mensaje,contacted = false,id = null,created_at = new Date().toISOString() ){
+    constructor(nombre,email,cell,servicio,mensaje){
         this.nombre = nombre;
         this.email = email;
         this.cell = cell;
         this.servicio = servicio;
         this.mensaje = mensaje;
+        // DEFAUL CONTROLADOS POR SISTEMA
         this.contacted = contacted;
-        this.id = id;
-        this.created_at = created_at;
+        this.id = null;
+        this.created_at = new Date().toDateString();
         
     }
 }
 
-let cliente1 = new Client("David Carrasco","davidack123456789@gmail.com","3134476364","Diseño web", "por favor contactame al cell para negociar")
-console.log(cliente1)
 
 /*VARIABLES GLOBALES DEL SISTEMA */
 
 let clientes =[]
-let nextID = 0
+let nextID = 1
 let emails = new Set ()
+
+/*FUNCION DONDE VALIDAMOS QUE TENGA TEXTO REAL */
+
+function inNonEmptyString(x){
+    return typeof x === "string" && x.trim().length > 0
+}
+
+
+/*FUNCION  */
+
+function guardarClientes(cliente){
+
+}
